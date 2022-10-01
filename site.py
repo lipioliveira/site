05446a20383eb0d6398567456d1d8248bcc9ac7d
lipioliveira <lipioliveira@gmail.com>
2022-10-01 13:38:29 -0300

    input_ok

--- site.py
+++ site.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n slash', 'Ps2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
 @app.route('/')
-def ola():
-    return render_template('lista.html')
+def index():
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome,categoria, console)
+    lista.append(jogo)
+    return redirect('/')
 
 
-app.run()
+app.run(debug=True)
